1d65fe96ed5c0792307cffca68ab484758bb3f6a
RhysF98 <rhys.furlow@gmail.com>
2019-02-16 16:52:51 +0000

    added search function

--- app/backend/searchjobs.py
+++ app/backend/searchjobs.py
@@ -3,3 +3,7 @@ from .models.py import Job
 def keyword_filter(keyword):
     results = Job.objects.filter(job_desc__contains(keyword))
     return results
+
+def title_search(title):
+    results = Job.objects.filter(job_title__contains(title))
+    return results

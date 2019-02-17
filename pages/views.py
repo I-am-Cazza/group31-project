bfaf807a508882efb447ad18e4f311e6b8628291
rush725 <rushabhmehta@Rushabhs-MBP.wifi-campus.warwick.ac.uk>
2019-02-17 17:30:23 +0000

    changed directory structure and added user signup functionality

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 # Create your views here.
 def index(request):
     context = {"home_page": "active"}
-    return render(request, 'pages/layouts/index.html', context)
+    return render(request, 'global/index.html', context)
 def signup(request):
+    form = UserCreationForm()
     # context = {'form': form}
     # return render(request, 'pages/layouts/signup.html', context)
     if request.method == 'POST':
@@ -18,11 +19,10 @@ def signup(request):
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('pages/layouts/index.html')
-    else:
-        form = UserCreationForm()
-    return render(request, 'pages/layouts/signup.html', {'form': form})
+            auth_login(request, user)
+            return render('global/index.html')
+    context={'form': form,'signup_page': 'active'}
+    return render(request, 'applicantportal/signup.html',context )
 def login(request):
     context = {"login_page": "active"}
-    return render(request, 'pages/layouts/login.html', context)
+    return render(request, 'applicantportal/login.html', context)

7b7e93ca20b5069f87acbc47c8d2e2561fe2fe16
rush725 <rushabhmehta@Rushabhs-MBP.wifi-campus.warwick.ac.uk>
2019-02-18 15:43:02 +0000

    added forms.py and registered models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Organisation
+from .models import Job
+from .models import TestQuestions
+
+admin.site.register(Organisation)
+admin.site.register(Job)
+admin.site.register(TestQuestions)
+
+
 # Register your models here.

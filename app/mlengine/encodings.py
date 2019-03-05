acc7679b98fec7856bf9e92e39affa4bc5f14eac
Cameron Hughes <cameronhughes0@btinternet.com>
2019-03-05 15:50:07 +0000

    Increased features considered by MLE

---
+++ app/mlengine/encodings.py
@@ -0,0 +1,9 @@
+class Encodings:
+    custom_indices: dict
+    uni_encodings: dict
+    degree_encodings: dict
+
+    def __init__(self):
+        self.custom_indices = dict()
+        self.uni_encodings = dict()
+        self.degree_encodings = dict()
